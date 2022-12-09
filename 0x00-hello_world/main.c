5ba189f25c3e054be1e61f34735558238c395132
BrownCarter <lilbrown3000@gmail.com>
2022-12-09 21:57:00 +0100

    added: main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,17 @@
+#include <assert.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-    return (0);
+struct Point {
+    int x;
+    int y;
+};
+
+int main() {
+    struct Point *points = NULL;
+    points->x = 23;
 }
