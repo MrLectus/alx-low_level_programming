e9486907f3c3d1b0b6f15888a92701f340cb6484
BrownCarter <lilbrown3000@gmail.com>
2022-12-10 01:40:13 +0100

    fix-4*

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i < 123; ++i)
+{
+if ((char)i == 'q' || (char)i == 'e')
+{
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
