3b3126eda2a21a49d93fba6c9eb430909e1b5caa
BrownCarter <lilbrown3000@gmail.com>
2022-12-13 14:16:38 +0100

    added 6-*

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - check lower case letter
+ * @c: character
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+		return (-c);
+
+	return (c);
+}
