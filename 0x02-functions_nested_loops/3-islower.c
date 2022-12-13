29d2d852b10feb0fc5b780345042b023bc6ecbd8
BrownCarter <lilbrown3000@gmail.com>
2022-12-13 13:39:35 +0100

    added 3-*

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check lower case letter
+ * @c: character
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
