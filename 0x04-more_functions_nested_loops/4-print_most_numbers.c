efb6f435ab79dc986e1758f99849bf5639650466
BrownCarter <lilbrown3000@gmail.com>
2022-12-16 02:19:24 +0100

    added 4-*

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,22 @@
-#include "_putchar.c"
 #include "main.h"
 
 /**
  * print_most_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
-void print_most_numbers(void)
+void print_most_numbers()
 {
-	char x;
+	int i;
 
-	for (x = '0'; x <= '9'; x++)
-		_putchar(x);
+	for (i = 48; i <= 57; ++i)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
 	_putchar('\n');
 }
