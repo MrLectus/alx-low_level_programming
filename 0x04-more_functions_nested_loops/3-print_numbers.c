aada3d72a8ee8c26c45b6964311ee3d520885604
BrownCarter <lilbrown3000@gmail.com>
2022-12-16 02:02:32 +0100

    -----------

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include "_putchar.c"
 /**
  * print_numbers - Entry point
  *
@@ -11,6 +12,6 @@ void print_numbers(void)
 	for (int i = 48; i <= 57; ++i)
 	{
 		_putchar(i);
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
