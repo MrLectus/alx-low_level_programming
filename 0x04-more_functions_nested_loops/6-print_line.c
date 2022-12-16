f9fe701d71c18fc6e1870a1afc56640fd1fb8734
BrownCarter <lilbrown3000@gmail.com>
2022-12-16 02:52:31 +0100

    -----------

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,6 @@
+#include "_putchar.c"
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_line - Entry point
  * @n: param 1
@@ -8,15 +9,21 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
 
 	int i;
 
 	for (i = 1; i <= n; ++i)
 	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
 		_putchar(95);
 	}
+	_putchar('\n');
+}
+
+int main()
+{
+	print_line(2);
 }
