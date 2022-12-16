c77aa93621dce1334f1a7fb061481da3d796fd35
BrownCarter <lilbrown3000@gmail.com>
2022-12-16 01:17:57 +0100

    added 2-mul*

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Entry point
+ * @a: param1
+ * @b: param1
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
