5e4d18c9120897bf93638ae61cf3382bf4b09d8e
BrownCarter <lilbrown3000@gmail.com>
2022-12-16 02:34:23 +0100

    fix 5-*

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
