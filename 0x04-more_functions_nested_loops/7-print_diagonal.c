e9a48adc35915ecd0f38df467ebc06b21f5286e0
BrownCarter <lilbrown3000@gmail.com>
2022-12-16 02:57:52 +0100

    added 7-*

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line
+ * @n: param 1
+ * Return: Always 0
+ **/
+
+void print_diagonal(int n)
+{
+	int first, second;
+
+	for (second = 1; second <= n; second++)
+	{
+		for (first = 1; first < second; first++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n < 1)
+		_putchar('\n');
+}
