7e5edc8f901b439f2e34d5d3f72474a99ef05c2b
BrownCarter <lilbrown3000@gmail.com>
2022-12-19 23:57:56 +0100

    added: 9-*

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print stdout
+ * @a: poniter 1
+ * @n: poniter 2
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
