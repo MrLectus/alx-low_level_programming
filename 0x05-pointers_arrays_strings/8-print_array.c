f6546843cd6867d7d1ea17aaec066bcd10d16b1d
BrownCarter <lilbrown3000@gmail.com>
2022-12-20 00:22:32 +0100

    fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; ++i)
 	{
-		printf("%d ", a[i]);
+		printf("%d", a[i]);
 		if (i < n - 1)
 		{
 			printf(",");
@@ -23,5 +23,6 @@ void print_array(int *a, int n)
 		{
 			continue;
 		}
+		printf(" ");
 	}
 }
