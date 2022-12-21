afb7836c94c135a83a1b51c6bcccd2500faaeb7b
BrownCarter <lilbrown3000@gmail.com>
2022-12-22 00:59:06 +0100

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,22 @@
-#include "../0x05-pointers_arrays_strings/2-strlen.c"
 #include "main.h"
 
+/**
+ * _strlen - find length
+ * @s: poniter 1
+ *
+ * Return: size
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
 /**
  * _strcat - concat string
  * @dest: poniter 1
@@ -12,9 +28,12 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
+
 	j = 0;
+
 	i = _strlen(dest);
+
 	while ((dest[i++] = src[j++]) != '\0')
 		;
-	return dest;
+	return (dest);
 }
