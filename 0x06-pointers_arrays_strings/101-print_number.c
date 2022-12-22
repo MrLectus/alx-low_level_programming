f865c6b45818f68f95995f20460f0090ce0a7c41
BrownCarter <lilbrown3000@gmail.com>
2022-12-22 11:16:33 +0100

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,7 +6,7 @@
  *
  */
 
-void print_number(size_t n)
+void print_number(int n)
 {
 	if (n < 0)
 	{
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-#include <stddef.h>
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -7,5 +6,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-void print_number(size_t n);
+void print_number(int n);
 int _putchar(char c);
