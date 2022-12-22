8814065143df77f48456910def24a7c241ad6652
BrownCarter <lilbrown3000@gmail.com>
2022-12-22 01:06:33 +0100

    added: 3-*

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "../0x05-pointers_arrays_strings/2-strlen.c"
+#include "main.h"
+
+/**
+ * _strcmp - copy string
+ * @s1: poniter 1
+ * @s2: poniter 2
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; ++i)
+	{
+		if (s1[i] == '\0')
+			return (0);
+	}
+	return (s1[i] - s2[i]);
+}
