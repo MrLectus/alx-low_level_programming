c8e097390135803d360963f8ca1d9045da408eb3
BrownCarter <lilbrown3000@gmail.com>
2022-12-22 03:54:01 +0100

    fix

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "../0x05-pointers_arrays_strings/1-swap.c"
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse array
+ * @a: poniter 1
+ * @n: value 2
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
