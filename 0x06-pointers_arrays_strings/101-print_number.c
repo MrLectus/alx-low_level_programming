c3697a84429ecdf765b8dfb6cab80f9786a8ee3e
BrownCarter <lilbrown3000@gmail.com>
2022-12-22 04:33:01 +0100

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,3 +1,4 @@
+#include "_putchar.c"
 #include "main.h"
 
 /**
@@ -8,5 +9,14 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 }
