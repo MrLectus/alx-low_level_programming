824e06923c551ca10bcbb7affd9bf2444a3c4cbd
BrownCarter <lilbrown3000@gmail.com>
2023-01-01 06:36:56 +0100

    New years first commit!

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - Entry point
+ * @s: param 1
+ * @b: param 2
+ * @n: param 3
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	long long i;
+
+	for (i = 0; i < n; ++i)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
