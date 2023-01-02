1044f05a37efdd189da6f749eed07c50abb73f98
BrownCarter <lilbrown3000@gmail.com>
2023-01-03 00:54:48 +0100

    added 3-*

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "2-strchr.c"
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - Entry point
+ * @s: param 1
+ * @accept: param 2
+ *
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	i = 0;
+
+	int count = 0;
+
+	if (s == NULL | accept == NULL)
+	{
+		return (0);
+	}
+
+	while (s[i] != '\0' && _strchr(accept, *s++))
+	{
+		count++;
+	}
+	return (count);
+}
