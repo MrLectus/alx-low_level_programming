ec9f2b34af4634f0d781d93f5f54c5183802f517
BrownCarter <lilbrown3000@gmail.com>
2023-01-03 00:47:37 +0100

    added 2-*

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Entry point
+ * @s: param 1
+ * @c: param 2
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+		else
+		{
+			s = (s + 1);
+		}
+	}
+	return (NULL);
+}
