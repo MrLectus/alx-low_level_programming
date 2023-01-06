7d2793cd5e112223df4ee936491138126134e654
BrownCarter <lilbrown3000@gmail.com>
2023-01-06 13:36:53 +0100

    added libmy

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc - 1);
+	return 0;
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 3)
+	{
+		int y = atoi(argv[2]);
+		int x = atoi(argv[1]);
+		printf("%d", x * y);
+	}
+	else
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	return 0;
+}
