f401fca6e53fa073361cfc97b14996fff75b5e15
BrownCarter <lilbrown3000@gmail.com>
2023-01-07 03:48:43 +0100

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,8 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -14,6 +16,8 @@ int main(int argc, char *argv[])
 {
 	int sum;
 	int i;
+	bool check;
+
 	sum = 0;
 
 	if (argc <= 0)
@@ -25,7 +29,22 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; ++i)
 		{
-			if (isdigit(*argv[i]))
+			size_t j;
+
+			for (j = 0; j < strlen(argv[i]); j++)
+			{
+				if (isdigit(argv[i][j]))
+				{
+					check = true;
+				}
+				else
+				{
+					check = false;
+					break;
+				}
+			}
+
+			if (check)
 			{
 				sum += atoi(argv[i]);
 			}
