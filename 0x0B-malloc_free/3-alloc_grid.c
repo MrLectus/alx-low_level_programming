1a8c0b168444b5ec9c61bb3914bef247ffe5d489
BrownCarter <lilbrown3000@gmail.com>
2023-01-10 03:18:35 +0100

    fic

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,33 +22,22 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grid = malloc((height) * sizeof(int *));
+	grid = (int **)malloc((height) * sizeof(int *));
+
 	if (grid == NULL)
 	{
-		free(grid);
 		return (NULL);
 	}
 
 	for (i = 0; i < height; ++i)
 	{
-		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
-		{
-			free(grid[i]);
-		}
+		grid[i] = (int *)malloc(width * sizeof(int));
 	}
 	for (i = 0; i < height; ++i)
 	{
-		if (grid[i] == NULL)
+		for (j = 0; j < width; ++j)
 		{
-			free(grid[i]);
-		}
-		else
-		{
-			for (j = 0; j < width; ++j)
-			{
-				grid[i][j] = 0;
-			}
+			grid[i][j] = 0;
 		}
 	}
 	return (grid);
